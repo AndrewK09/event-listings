ef386ef2ff112eba171cb3e5f414f25290cccc63
Andrew <Andrewkn09@Gmail.com>
2020-03-10 17:13:56 -0400

    Set json data as event state

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { json } from '../shared/data.js';
 
 function App() {
+  const [event, updateEvents] = useState(json.Items);
+
   return (
     <div>
       <h1>Elton John Listings</h1>
