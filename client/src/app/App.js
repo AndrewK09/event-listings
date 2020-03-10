221f9d8a87cb5367099b4f5d8281f53fe702c218
Andrew <Andrewkn09@Gmail.com>
2020-03-10 17:46:47 -0400

    Incorporate styles

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 import EventList from '../features/eventList_pt1/EventList.js';
-
+import styles from './App.css';
 import { json } from '../shared/data.js';
 
 function App() {
   const [events, updateEvents] = useState(json.Items);
-
+  const { MainContainer, Title } = styles;
   return (
-    <div>
-      <h1>Elton John Listings</h1>
+    <div className={MainContainer}>
+      <h1 className={Title}>Elton John Listings</h1>
       <EventList events={events} />
     </div>
   );
--- client/src/features/eventList_pt1/eventList.js
+++ client/src/features/eventList_pt1/eventList.js
@@ -1,21 +1,22 @@
 import React from 'react';
+import styles from './EventList.css';
 
 const EventList = ({ events }) => {
+  const { container, row } = styles;
   return events ? (
-    <div>
+    <div className={container}>
       {events.map(event => {
         return (
-          <div key={event.EventId}>
+          <div className={row} key={event.EventId}>
             <div>
-              <p>
-                {event.VenueCity}, {event.VenueCountry}
-              </p>
               <p>{event.Date}</p>
               <p>{event.Time}</p>
             </div>
 
             <div>
-              <p>{event.VenueName}</p>
+              <p>
+                {event.VenueName}, {event.VenueCity}, {event.VenueCountry}
+              </p>
               <p>{event.VenueCapacity}</p>
               <p>{event.EventName}</p>
               <p>{event.MinPrice}</p>
