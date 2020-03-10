e9f4632cf8d0893468b24cf883cad9b5e96e8f3a
Andrew <Andrewkn09@Gmail.com>
2020-03-10 18:37:55 -0400

    Add conditional style rendering

--- client/src/features/eventList_pt1/eventList.js
+++ client/src/features/eventList_pt1/eventList.js
@@ -1,8 +1,18 @@
 import React from 'react';
-import styles from './EventList.css';
+import {
+  container,
+  row,
+  col1,
+  col2,
+  col3,
+  col4,
+  isSellingFast,
+  isNotSellingFast,
+  isCheapestCity,
+  isNotCheapestCity,
+} from './EventList.css';
 
 const EventList = ({ events }) => {
-  const { container, row, col1, col2 } = styles;
   return events ? (
     <div className={container}>
       {events.map(event => {
@@ -11,14 +21,35 @@ const EventList = ({ events }) => {
             <div className={col1}>
               <span>{event.Date}</span>
               <span>{event.Time}</span>
+              <span
+                className={
+                  event.IsSellingFast ? isSellingFast : isNotSellingFast
+                }
+              >
+                Tickets Selling Fast!!
+              </span>
             </div>
 
             <div className={col2}>
               <span>
                 {event.VenueName}, {event.VenueCity}, {event.VenueCountry}
               </span>
-              <span>{event.VenueCapacity}</span>
+              <span>Venue Capacity: {event.VenueCapacity}</span>
               <span>{event.EventName}</span>
+              <span
+                className={
+                  event.IsCheapestCity ? isCheapestCity : isNotCheapestCity
+                }
+              >
+                {event.IsCheapestCityContent}
+              </span>
+            </div>
+
+            <div className={col3}>
+              <span>{event.AvailableTickets}</span>
+            </div>
+
+            <div className={col4}>
               <span>{event.MinPrice}</span>
             </div>
           </div>
