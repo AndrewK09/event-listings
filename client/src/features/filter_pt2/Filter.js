041930461cc7f385de9c8199e72c477ae124cf22
Andrew <Andrewkn09@Gmail.com>
2020-03-10 19:50:28 -0400

    Implement Filter component

--- client/src/features/filter_pt2/Filter.js
+++ client/src/features/filter_pt2/Filter.js
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { json } from '../../shared/data.js';
+import { Container } from './Filter.css';
+
+//converts list of events to list of event cities
+const eventCities = json.Items.map(event => {
+  return event.VenueCity;
+});
 
 const Filter = props => {
-  return <div>Search By Location:</div>;
+  const [selected, updateSelected] = useState('All Cities');
+
+  //Update selected value and  updates list of events
+  const handleSelect = e => {
+    const city = e.target.value;
+    updateSelected(city);
+    filterEventsByCity(city);
+  };
+
+  const filterEventsByCity = city => {
+    let filteredEvents;
+
+    if (city === 'All Cities') {
+      filteredEvents = json.Items;
+    } else {
+      filteredEvents = json.Items.filter(event => {
+        return event.VenueCity === city;
+      });
+    }
+
+    props.updateEvents(filteredEvents);
+  };
+
+  return (
+    <form className={Container}>
+      <label>Search By Location:</label>
+
+      <select value={selected} onChange={handleSelect}>
+        <option value='All Cities'>All Cities</option>
+
+        {eventCities.map((city, i) => {
+          return (
+            <option value={city} key={i}>
+              {city}
+            </option>
+          );
+        })}
+      </select>
+    </form>
+  );
 };
 
 export default Filter;
