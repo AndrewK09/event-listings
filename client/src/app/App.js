73af2da1e5cd2b151a5b10c727b0fbf834f1a3c5
Andrew <Andrewkn09@Gmail.com>
2020-03-10 19:48:19 -0400

    Utilize Filter component

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 import EventList from '../features/eventList_pt1/EventList.js';
+import Filter from '../features/filter_pt2/Filter.js';
+
 import styles from './App.css';
 import { json } from '../shared/data.js';
 
 function App() {
   const [events, updateEvents] = useState(json.Items);
+
   const { MainContainer, Title } = styles;
   return (
     <div className={MainContainer}>
       <h1 className={Title}>Elton John Listings</h1>
+      <Filter updateEvents={updateEvents} />
       <EventList events={events} />
     </div>
   );
