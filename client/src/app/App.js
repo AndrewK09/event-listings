1ce23553b176f9f14c7b23012f7dcb956af065f1
Andrew <Andrewkn09@Gmail.com>
2020-03-10 17:33:13 -0400

    Render EventList component

--- client/src/app/App.js
+++ client/src/app/App.js
@@ -1,12 +1,15 @@
 import React, { useState } from 'react';
+import EventList from '../features/eventList_pt1/EventList.js';
+
 import { json } from '../shared/data.js';
 
 function App() {
-  const [event, updateEvents] = useState(json.Items);
+  const [events, updateEvents] = useState(json.Items);
 
   return (
     <div>
       <h1>Elton John Listings</h1>
+      <EventList events={events} />
     </div>
   );
 }
